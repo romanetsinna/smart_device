b00a22ad8908abf550d1c39ae99ce9333f94077c
Инна Романец <romanets_inna@outlook.com>
2023-03-20 09:46:57 +0300

    Правки после код ревью

--- source/js/main.js
+++ source/js/main.js
@@ -54,20 +54,24 @@ window.addEventListener('DOMContentLoaded', () => {
 
 const boxes = Array.from(document.querySelectorAll('.accordion__box'));
 
-boxes.forEach((box) => {
-  box.addEventListener('click', boxHandler);
-});
+if (boxes) {
+  boxes.forEach((box) => {
+    box.addEventListener('click', boxHandler);
+  });
+}
 
 function boxHandler(e) {
   hidePanels();
   e.preventDefault();
   let currentBox = e.target.closest('.accordion__box');
   let currentContent = e.target.nextElementSibling;
-  currentBox.classList.add('active');
-  if (currentBox.classList.contains('active')) {
-    currentContent.style.maxHeight = currentContent.scrollHeight + 'px';
-  } else {
-    currentContent.style.maxHeight = 0;
+  if (currentBox && currentContent) {
+    currentBox.classList.add('active');
+    if (currentBox.classList.contains('active')) {
+      currentContent.style.maxHeight = currentContent.scrollHeight + 'px';
+    } else {
+      currentContent.style.maxHeight = 0;
+    }
   }
 }
 
@@ -85,65 +89,10 @@ function hidePanels() {
 let moreText = document.querySelector('.about__show-more');
 let btnText = document.querySelector('.about__button');
 
-btnText.addEventListener('click', function (event) {
-  event.preventDefault();
-  moreText.classList.toggle('about__show-more');
-  btnText.textContent = btnText.textContent === 'Подробнее' ? 'Свернуть' : 'Подробнее';
-});
-
-
-// Плавный скролл
-
-const smoothLinks = document.querySelectorAll('a[href^="#"]');
-
-for (let smoothLink of smoothLinks) {
-  smoothLink.addEventListener('click', function (e) {
-    e.preventDefault();
-    const id = smoothLink.getAttribute('href');
-
-    document.querySelector(id).scrollIntoView({
-      behavior: 'smooth',
-      block: 'start',
-    });
+if (moreText && btnText) {
+  btnText.addEventListener('click', function (event) {
+    event.preventDefault();
+    moreText.classList.toggle('about__show-more');
+    btnText.textContent = btnText.textContent === 'Подробнее' ? 'Свернуть' : 'Подробнее';
   });
 }
-
-// Маска для ввода телефона
-
-// function maskPhone(selector, masked = '+7 (___) ___-__-__') {
-//   const elems = document.querySelectorAll('.tel');
-
-//   function mask(event) {
-//     const keyCode = event.keyCode;
-//     const template = masked;
-//     let def = template.replace(/\D/g, '');
-//     let val = event.target.value.replace(/\D/g, '');
-//     let i = 0;
-//     let newValue = template.replace(/[_\d]/g, function (a) {
-//       return i < val.length ? val.charAt(i++) || def.charAt(i) : a;
-//     });
-//     i = newValue.indexOf('_');
-//     if (i !== -1) {
-//       newValue = newValue.slice(0, i);
-//     }
-//     let reg = template.substr(0, event.target.value.length).replace(/_+/g,
-//         function (a) {
-//           return '\\d{1,' + a.length + '}';
-//         }).replace(/[+()]/g, '\\$&');
-//     reg = new RegExp('^' + reg + '$');
-//     if (!reg.test(event.target.value) || event.target.value.length < 5 || keyCode > 47 && keyCode < 58) {
-//       event.target.value = newValue;
-//     }
-//     if (event.type === 'blur' && event.target.value.length < 5) {
-//       event.target.value = '';
-//     }
-//   }
-
-//   for (const elem of elems) {
-//     elem.addEventListener('input', mask);
-//     elem.addEventListener('focus', mask);
-//     elem.addEventListener('blur', mask);
-//   }
-// }
-
-// maskPhone();
